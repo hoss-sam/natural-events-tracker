b9e3f1d5d05187bbb39e33fab6284da024525fb8
Hossam Ali <hossam.alawadly@gmail.com>
2021-01-26 01:28:15 +0200

    Updated tile layer to Esri.WorldTopoMap

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -5,8 +5,8 @@ const Maps =({eventData}) => {
     return (
         <MapContainer center={[40.04449, -105.23574]} zoom={5} scrollWheelZoom={true}>
             <TileLayer 
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+            url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}"
             />
             {/* <Marker position={[40.04449, -105.23574]}>
                 <Popup>
